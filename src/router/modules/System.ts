99b55d64424177639bb30ff58feb2531005798c7
taoshuaixing <1554307063@qq.com>
2022-09-20 15:04:21 +0800

    add:login page

--- src/router/modules/System.ts
+++ src/router/modules/System.ts
@@ -1,6 +1,30 @@
 import { RouteRecordRaw } from 'vue-router';
 import Layout from '@/layout/index.vue';
 
-const System: Array<RouteRecordRaw> = [];
+const System: Array<RouteRecordRaw> = [
+	{
+		path: '/system',
+		component: Layout,
+		redirect: '/404',
+		meta: {
+			title: '系统目录',
+			showMenu: true.valueOf,
+			icon: 'icon-dicengjiagou',
+		},
+		children: [
+			{
+				path: '/404',
+				component: () => import('@/views/system/404.vue'),
+				meta: { title: '404页面' },
+			},
+			{
+				path: '/login',
+				name: 'login',
+				meta: { title: '登陆页面', icon: '', showMenu: false },
+				component: () => import('@/views/login/index.vue'),
+			},
+		],
+	},
+];
 
 export default System;
