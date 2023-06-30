fe1dbfe695cf4a3307d3420b13d45f529194eb1b
Taoshuaixing <1554307063@qq.com>
2023-06-30 09:53:28 +0800

    build:跨域处理

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@
  * @Author: 陶帅星
  * @Date: 2022-11-17 17:21:33
  * @LastEditors: 陶帅星
- * @LastEditTime: 2023-06-29 19:26:07
+ * @LastEditTime: 2023-06-30 09:53:25
  */
 import { resolve } from 'path'
 import { UserConfigExport, ConfigEnv } from 'vite'
@@ -63,22 +63,6 @@ export default ({ command }: ConfigEnv): UserConfigExport => {
         ]
       })
     ],
-    server: {
-      host: '0.0.0.0',
-      port: 8080,
-      open: false,
-      cors: true, // 允许跨域
-      proxy: {
-        '/api': {
-          // target: '',
-          target: 'http://admin.xueyueob.cn/api',
-          changeOrigin: true,
-          ws: true,
-          // rewrite: (path) => path.replace(new RegExp('^/api'), '')
-          rewrite: (path) => path.replace(/^\/api/, '')
-        }
-      }
-    },
     resolve: {
       alias: {
         '@': pathReslove('./src'),
@@ -87,10 +71,12 @@ export default ({ command }: ConfigEnv): UserConfigExport => {
       }
     },
     build: {
-      // sourcemap: true,
-      manifest: true,
-      minify: 'terser',
-      chunkSizeWarningLimit: 500, // 提高静态资源大小警告
+      cssCodeSplit: true, // 如果设置为false，整个项目中的所有 CSS 将被提取到一个 CSS 文件中
+      sourcemap: false, // 构建后是否生成 source map 文件。如果为 true，将会创建一个独立的 source map 文件
+      target: 'modules', // 设置最终构建的浏览器兼容目标。默认值是一个 Vite 特有的值——'modules'  还可设置为 'es2015' 'es2016'等
+      chunkSizeWarningLimit: 550, // 单位kb  打包后文件大小警告的限制 (文件大于此此值会出现警告)
+      assetsInlineLimit: 4096, // 单位字节（1024等于1kb） 小于此阈值的导入或引用资源将内联为 base64 编码，以避免额外的 http 请求。设置为 0 可以完全禁用此项。
+      minify: 'terser', // 'terser' 相对较慢，但大多数情况下构建后的文件体积更小。'esbuild' 最小化混淆更快但构建后的文件相对更大。
       terserOptions: {
         // 清除console和debugger
         compress: {
@@ -105,6 +91,22 @@ export default ({ command }: ConfigEnv): UserConfigExport => {
             'element-plus': ['element-plus'],
             echarts: ['echarts']
           }
+        }
+      }
+    },
+    server: {
+      host: '0.0.0.0',
+      port: 8080,
+      open: false,
+      cors: true, // 允许跨域
+      proxy: {
+        '/api': {
+          target: 'http://localhost:8080',
+          changeOrigin: true,
+          ws: true,
+          secure: false,
+          // rewrite: (path) => path.replace(new RegExp('^/api'), '')
+          rewrite: (path) => path.replace('/api/', '/')
         }
       }
     }
